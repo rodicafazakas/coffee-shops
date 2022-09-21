d7b3a4fd50ee777eacad047f7f71fe164c392139
Rodica Fazakas <rodica.fazakas@int.softonic.com>
2022-09-21 13:05:57 +0200

    feat: add SSG prerendering of dynamic routes

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,15 +1,47 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+export function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find( (coffeeStore) => {
+        return coffeeStore.id.toString() === params.id
+      })
+    }
+  }
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" }}],
+    fallback: false,
+  }
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
   const { id } = router.query;
+
+  if (props.fallback) {
+    return <div>Loading ...</div>
+  }
+
+  const { address, name, neighbourhood } = props.coffeeStore; 
+
   return (
     <div>
-      Coffee Store Page {id}
+      <Head>
+        <title>{name}</title>
+      </Head>
       <Link href="/">
         <a> back to home </a>
       </Link> 
+      <p>{name}</p>
+      <p>{address}</p>
+      <p>{neighbourhood}</p>
     </div>
   )
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,17 @@ import styles from '../styles/Home.module.css'
 import Banner from "../components/banner";
 import Card from "../components/card";
 
-import coffeeStores from "../data/coffee-stores.json";
+import coffeeStoresData from "../data/coffee-stores.json";
 
 export async function getStaticProps() {
   return {
-    props: {coffeeStores}
+    props: {
+      coffeeStores: coffeeStoresData
+    }
   }
 }
 
 const Home: NextPage = (props) => {
-  console.log("props", props);
 
   const handleOnBannerBtnClick =() => {
     console.log("hi banner button");
@@ -35,23 +36,29 @@ const Home: NextPage = (props) => {
           handleOnClick={handleOnBannerBtnClick} 
         />
 
-        <div className={styles.heroImage}>
+        {/* <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" alt="hero-image" width={700} height={400}/>
-        </div>
-        
-        <div className="cardLayout">
-          {coffeeStores.map( (coffeeStore) => {
-            return (
-              <Card 
-                key={coffeeStore.id}
-                name={coffeeStore.name}
-                imgUrl={coffeeStore.imgUrl} 
-                href={`/coffee-store/${coffeeStore.id}`} 
-                className={styles.card}
-              />
-            )}
-          )}
-        </div>
+        </div> */}
+
+        {props.coffeeStores.length >0 && (
+          <>
+            <h2 className={styles.heading2}> Toronto stores </h2>
+            
+            <div className="cardLayout">
+              {props.coffeeStores.map( (coffeeStore) => {
+                return (
+                  <Card 
+                    key={coffeeStore.id}
+                    name={coffeeStore.name}
+                    imgUrl={coffeeStore.imgUrl} 
+                    href={`/coffee-store/${coffeeStore.id}`} 
+                    className={styles.card}
+                  />
+                )}
+              )}
+            </div>
+          </>
+        )}
 
       </main>
     </div>
